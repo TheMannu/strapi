df092af97afb069766629f8ee648c136487d90a7
TheMannu <mannuveerashwan@gmail.com>
2024-07-07 01:08:57 +0530

    Output Modified

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,10 +6,6 @@ output "public_ip" {
   value = aws_instance.strapi.public_ip
 }
 
-output "instance_ip" {
-  value = aws_instance.strapi.public_ip
-} 
-
 output "public_dns" {
   value = aws_instance.strapi.public_dns
 }
